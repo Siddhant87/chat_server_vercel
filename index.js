49fc842011cad4553aab1fee324c1d98a9a09e59
Siddhant87 <siddhantkumar87@gmail.com>
2025-01-24 23:11:48 +0530

    'commit'

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-
-
-
 // nodemon is used which automatically restarts your node application when it detects any changes
 
-const express = require("express");
-const {createServer} = require("http");
-const {Server} = require("socket.io");
+// use 'npm run dev' to run server
 
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const httpserver = createServer(app);
+const httpserver = http.createServer(app);
 const io = new Server(httpserver);
 
-const cors = require('cors');
+const cors = require("cors");
 
 //const port = process.env.PORT || 3000;
 
@@ -20,19 +18,44 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-
-app.route("/").get((req , res)=>{
-    res.json("hey welcome to server");
-})
-
-
-io.on("connection",(socket)=>{
-    console.log("backend connected");
-    socket.on("sendMsg",(msg)=>{
-        console.log(msg);
-    });
+app.route("/").get((req, res) => {
+  res.json("Hey welcome to server...");
 });
 
-httpserver.listen(3000,()=>{
-    console.log("server started");
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('disconnect', () => {
+//       console.log('User disconnected');
+//     });
+//     socket.on('chat message', (msg) => {
+//       console.log(`Message: ${msg}`);
+//       io.emit('chat message', msg); // Broadcast the message to all connected clients
+//     });
+//   });
+
+io.on("connection", function (socket) {
+  console.log("backend connected");
+  socket.join("anonymous_group");
+  socket.on("sendMsg", (msg) => {
+    console.log(msg);
+    io.to("anonymous_group").emit("sendMsgServer", {
+      ...msg,
+      type: "otherMsg",
+    });
+
+    // broadcast sends to all users except sender
+    // socket.broadcast.to("anonymous_group").emit("sendMsgServer", {
+    //   ...msg,
+    //   type: "otherMsg",
+    // });
+    //  socket.emit("sendMsgServer" , {...msg, type:"otherMsg"}); //copy the whole msg json and overwrite the type
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+httpserver.listen(3000, () => {
+  console.log("server started");
 });
