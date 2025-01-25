1b10c87f1f04ec56510bf617087b6d5c87901617
Siddhant87 <siddhantkumar87@gmail.com>
2025-01-26 00:14:58 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
-import express from "express";
+// nodemon is used which automatically restarts your node application when it detects any changes
+
+// use 'npm run dev' to run server
+
+/*
+
+
+
+
+
+*/
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const port = 9000;
-app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+const httpserver = http.createServer(app);
+const io = new Server(httpserver);
+
+const cors = require("cors");
+
+//const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.route("/").get((req, res) => {
+  res.json("Hey welcome to server...");
 });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('disconnect', () => {
+//       console.log('User disconnected');
+//     });
+//     socket.on('chat message', (msg) => {
+//       console.log(`Message: ${msg}`);
+//       io.emit('chat message', msg); // Broadcast the message to all connected clients
+//     });
+//   });
+
+io.on("connection", function (socket) {
+  console.log("backend connected");
+  socket.join("anonymous_group");
+  socket.on("sendMsg", (msg) => {
+    console.log(msg);
+    io.to("anonymous_group").emit("sendMsgServer", {
+      ...msg,
+      type: "otherMsg",
+    });
+
+    // broadcast sends to all users except sender
+    // socket.broadcast.to("anonymous_group").emit("sendMsgServer", {
+    //   ...msg,
+    //   type: "otherMsg",
+    // });
+    //  socket.emit("sendMsgServer" , {...msg, type:"otherMsg"}); //copy the whole msg json and overwrite the type
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+httpserver.listen(3000, () => {
+  console.log("server started");
 });
